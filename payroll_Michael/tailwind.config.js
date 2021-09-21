02f022a217abcda5ca5d1a498a00f8e0e0a71b37
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-09-21 10:44:47 +0800

    Add files via upload

---
+++ payroll_Michael/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: []
+}
