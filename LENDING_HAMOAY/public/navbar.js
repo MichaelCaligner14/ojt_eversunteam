6c4a6b0547a0515d6309ccec965bac4c083ed3f7
TrishiaHamo-ay <89622810+TrishiaHamo-ay@users.noreply.github.com>
2021-09-23 10:46:03 +0800

    Delete navbar.js

--- LENDING_HAMOAY/public/navbar.js
+++
@@ -1,8 +0,0 @@
-const btnn = document.querySelector("button.button");
-const menu = document.querySelector(".mobile");
-
-btnn.addEventListener("click" , () => {
-    menu.classList.toggle("hidden");
-}
-
-);
