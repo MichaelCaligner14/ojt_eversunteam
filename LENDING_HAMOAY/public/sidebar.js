f2fde04f04b7922aa4ea3f47a45a0ffbf2561b9c
TrishiaHamo-ay <89622810+TrishiaHamo-ay@users.noreply.github.com>
2021-09-23 10:45:50 +0800

    Delete sidebar.js

--- LENDING_HAMOAY/public/sidebar.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector(".mobile-menu");
-const sidebar = document.querySelector(".sidebar");
-
-btn.addEventListener("click" , () => {
-    sidebar.classList.toggle("-translate-x-full");
-});
-
-
