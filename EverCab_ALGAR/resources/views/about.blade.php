c25d0f0860de5683c8fa2cfe13d1f7d68675c969
KentJosephAlgar <89622322+KentJosephAlgar@users.noreply.github.com>
2021-09-24 09:38:35 +0800

    Add files via upload

---
+++ EverCab_ALGAR/resources/views/about.blade.php
@@ -0,0 +1,109 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <div>
+        <div class="text-dark-gray text-2xl m-4">
+            <a href="{{url('welcome')}}" class="hover:underline">Home</a>
+            <a href="{{url('login')}}" class="ml-4 hover:underline">Login</a>
+        </div>
+        <div class="text-6xl font-bold font-condensed mt-20 grid grid-cols-3">
+            <h3 class="col-start-2 lg:ml-20">One Team, One Goal</h3>
+            <h3 class="col-start-2 lg:ml-28">Our Time Is Now!</h3>
+        </div>
+        <img src="img/handsphones.png" alt="hands" class="w-full">
+    </div>
+    <div class="grid grid-cols-3 text-white bg-dark-blue static">
+        <div class="col-start-1 col-span-1 mt-10 ml-20 mb-10 text-3xl font-bold font-condensed">
+            Who We Are
+        </div>
+        <div class="col-start-2 col-span-2 font-roboto mt-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+    </div>
+    <div class="text-dark-gray">
+        <div class="text-2xl font-bold mt-10 ml-10 mb-5">
+            Meet the Founders and Partners
+        </div>
+        <div class="text-xl ml-10 mb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+    </div>
+    <div class="grid md:grid-cols-3 md:gap-x-20 gap-y-40 text-xl font-bold">
+        <div>
+            <img src="img/founder1.png" alt="found1" class="ml-20 md:ml-16 lg:ml-20">
+            <span class="ml-44 md:ml-32 lg:ml-44">Co-Founder</span>
+        </div>
+        <div>
+            <img src="img/founder2.png" alt="found2" class="ml-20 md:ml-12">
+            <span class="ml-44 md:ml-28 lg:ml-36">Co-Founder</span>
+        </div>
+        <div>
+            <img src="img/partner1.png" alt="part1" class="ml-20 md:ml-8">
+            <span class="ml-48 md:ml-32 lg:ml-36">Partner</span>
+        </div>
+    </div>
+    <div class="grid md:grid-cols-3 md:gap-x-20 gap-y-40 text-xl font-bold mt-20">
+        <div>
+            <img src="img/partner2.png" alt="part2" class="ml-20 md:ml-16 lg:ml-20">
+            <span class="ml-48 md:ml-36 lg:ml-48">Partner</span>
+        </div>
+        <div>
+            <img src="img/leadfrontend.png" alt="front" class="ml-20 md:ml-12">
+            <span class="ml-40 md:ml-24 lg:ml-28">PR Lead Frontend</span>
+        </div>
+        <div>
+            <img src="img/cto.png" alt="chief" class="ml-20 md:ml-8">
+            <span class="ml-44 md:ml-24 lg:ml-28">Partner & CTO</span>
+        </div>
+    </div>
+    <div class="grid grid-cols-4 md:text-3xl font-bold mt-10 bg-dark-blue text-white">
+        <span class="ml-10 mt-10">
+            EverCab
+            <span class="block text-sm font-semibold mt-5">
+                ©Copyright 2021. All Rights Reserved.
+            </span>
+        </span>
+        <span class="mt-10">
+            Ride
+            <span class="block text-sm font-semibold mt-5">
+                Drivers
+            </span>
+            <span class="block text-sm font-semibold mt-5">
+                Safety
+            </span>
+        </span>
+        <span class="mt-10">
+            Meet The Team
+            <span class="block text-sm font-semibold mt-5">
+                Developers
+            </span>
+            <span class="block text-sm font-semibold mt-5">
+                Business Partners
+            </span>
+        </span>
+        <span class="mt-10">
+            About Us
+            <span class="block text-sm font-semibold mt-5">
+                Contact
+            </span>
+            <span class="block text-sm font-semibold mt-5">
+                Objectives
+            </span>
+            <span class="block text-sm font-semibold mt-5">
+                Social Media
+            </span>
+        </span>
+    </div>
+    <div class="text-5xl font-bold grid grid-cols-5 bg-dark-blue text-white">
+        <span class="col-start-2 md:col-start-3 m-20 md:-ml-8">
+            EverCab
+        </span>
+    </div>
+</body>
+</html>
