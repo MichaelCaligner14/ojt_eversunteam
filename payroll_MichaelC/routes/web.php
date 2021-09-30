1affa61c39b933072f7b38061140518d1c4980f3
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-09-30 17:43:58 +0800

    Add files via upload

---
+++ payroll_MichaelC/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\Employeelist;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login',[CustomAuthController::class,'login'])->middleware('alreadyLoggedIn');
+Route::get('/registration',[CustomAuthController::class,'registration']);
+Route::post('/register-user',[CustomAuthController::class,'registerUser'])->name('register-user');
+Route::post('/login-user',[CustomAuthController::class,'loginUser'])->name('login-user');
+Route::get('/logout',[CustomAuthController::class,'logout']);
+
+Route::get('/employee',[Employeelist::class,'employee'])->middleware('isLoggedIn');
+Route::post('/add',[Employeelist::class,'add']);
+Route::get('delete/{id}',[Employeelist::class,'delete']);
+
+Route::get('edit/{id}',[Employeelist::class,'edit']);
+
+
+Route::get('/attendance',[Employeelist::class,'attendance']);
