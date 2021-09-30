8d29a7113d5e6790233279e161b51fa33b6e0cd2
KentJosephAlgar <89622322+KentJosephAlgar@users.noreply.github.com>
2021-09-30 17:53:57 +0800

    Add files via upload

---
+++ EverCab_ALGAR/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+use Session;
+
+class CustomAuthController extends Controller
+{
+    public function login(){
+        return view("auth.login");
+    }
+    public function register(){
+        return view("auth.register");
+    }
+    public function registerUser(Request $request){
+        $request->validate([
+            'email'=>'required|email|unique:users',
+            'name'=>'required',
+            'username'=>'required',
+            'password'=>'required|min:5|max:12'
+        ]);
+        $user = new User();
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->password = Hash::make($request->password);
+        $res = $user->save();
+        if($res){
+            return back()->with('success', 'You have registered successfully!');
+        }else{
+            return back()->with('fail', 'Something went wrong!');
+        }
+    }
+    public function loginUser(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required|min:5|max:12'
+        ]);
+        $user = User::where('email', '=', $request->email)->first();
+        if($user){
+            if(Hash::check($request->password, $user->password)){
+                $request->session()->put('loginId', $user->id);
+                return redirect('dashboard');
+            }else{
+                return back()->with('fail', 'The password does not match.');
+            }
+        }else{
+            return back()->with('fail', 'This email is not registered.');
+        }
+    }
+    public function dashboard(){
+        $data = array();
+        if(Session::has('loginId')){
+            $data = User::where('id', '=', Session::get('loginId'))->first();
+        }
+        return view('dashboard', compact('data'));
+    }
+    public function logout(){
+        if(Session::has('loginId')){
+            Session::pull('loginId');
+            return redirect('login');
+        }
+    }
+}
