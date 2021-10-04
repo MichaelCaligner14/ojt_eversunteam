3797c843292bef3043f2d2cfc84a2958617b288c
pinedajamesryan09 <89622639+pinedajamesryan09@users.noreply.github.com>
2021-10-04 18:01:51 +0800

    Add files via upload

---
+++ student_registration_James/resources/views/welcome.blade.php
@@ -0,0 +1,76 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start Page</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body>
+
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initials-scale=1.0">
+    <title>School Registration</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body class="mx-auto">
+ 
+    <div
+      class="px-96 w-screen h-screen flex flex-col justify-center items-center lg:grid lg:grid-cols-2"
+    >
+    
+      
+      
+      <div class=" md:grid-flow-col mx-28 bg-gray-800 rounded-lg" style="width: 400px; height: 600px; float: right: 150px;"/>
+      <form class="bg-casal-500 w-full h-screen flex flex-col justify-center items-center w-1/2">
+        <img src="./logo.png"/ style="width: 200px; height: 200px;">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+            </svg>
+            
+        <h2
+          class="my-8 font-roboto font-bold text-3xl text-white text-center"
+        >
+        
+        WELCOME STUDENTS!
+        </h2>
+        
+          <div class="flex mt-9 mx-28 text-white text-center font-bold">
+            
+              <span class="ml-3">New Students:</span></span>
+            </label>
+          </div>
+          
+        <br>
+        <a href="registration"
+          class="py-1 px-5 bg-primarycolor rounded-7 mx-28 text-white font-bold uppercase text-lg mt-4 transform bg-black-600 transition-all duration-500 bg-red-600 bg-opacity-90 rounded" style="width: 125px; height: 100px;"
+          >REGISTER</a
+        > 
+
+        <div class="flex mt-6 text-white mx-28 font-bold">
+            
+            <span class="ml-3">Old Student:</span></span>
+          </label>
+        </div>
+        <Br>
+      
+      <a
+      
+        href="login"
+        class="py-1 px-3 bg-primarycolor rounded-5 mx-32 text-white font-bold uppercase text-lg mt-4 transform bg-black-600 transition-all duration-500 bg-blue-700 bg-opacity-90 rounded" style="width: 80px; height: 37px;"
+        >LOGIN</a
+      > 
+          
+        
+      </form>
+    </div>
+
+</body>
+</html>
+    
+</body>
+</html>
