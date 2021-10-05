61bcfe87eea6013ba96ab9fd387de1ea0c432cda
AdrianS09 <89622775+AdrianS09@users.noreply.github.com>
2021-10-05 17:41:03 +0800

    Delete inventory_adrian/public directory

--- inventory_adrian/public/empdeletemodal.js
+++
@@ -1,9 +0,0 @@
-
-    const buttons = document.querySelectorAll("#deletemodal");
-
-    buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        modal.classList.add('scale-100');
-    });
-    closebutton.addEventListener('click',()=>modal.classList.remove('scale-100'))
-})
