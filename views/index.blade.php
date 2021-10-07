8cd86c938133cc84f64ca7144d40b74e784558cc
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-10-07 17:51:28 +0800

    Add files via upload

---
+++ views/index.blade.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html>
+<head>
+  <!-- ... -->
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet/less" type="text/css" href="css/style.less">
+<script src="https://cdn.jsdelivr.net/npm/less@4.1.1" ></script>
+  <title>Payroll System</title>
+</head>
+<body style="background-image: url('img/bg1.png');">
+<div class="text-8xl text-center font-bold py-56">
+    <h1 class=" text-gray-700">PAYROLL SYSTEM</h1>
+    <div class="justify-center py-24 gap-6">
+   <a href="login"><button class="btnlog" type="submit">
+              Login
+              </button></a>
+  <a href="registration"><button class="btnreg" type="submit">
+              Signup
+              </button></a>
+</div>
+</body>
+</html>
