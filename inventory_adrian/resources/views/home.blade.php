39c3e189652e1282ac782ec83aa50e9a8e5cdb2d
AdrianS09 <89622775+AdrianS09@users.noreply.github.com>
2021-10-11 17:40:51 +0800

    Add files via upload

---
+++ inventory_adrian/resources/views/home.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.homenavbar')
+
+@section('content')
+            <div class="flex justify-center font-bold pt-16 pb-16">
+            <!--DASHBOARD-->
+
+            <div class="flex flex-wrap justify-center gap-x-32">
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                     <svg class="m-6 h-52" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                      </svg> 
+                        <a href="user" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">User</a>
+                </div>
+
+                <div class="flex justify-center gap-x-32">
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                    <svg class="m-6 h-52" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                      </svg>
+                    <a href="order" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">Order</a>
+                
+                </div>
+
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                    <svg class="m-6 h-52" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                      </svg>
+                
+                    <a href="category" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">Category</a>
+                
+                </div>
+            </div>
+
+            <div class="flex flex-wrap justify-center gap-x-32">
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                <svg class="m-6 h-52" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+                    <a href="employee" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">Employee</a>
+                
+                </div>
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                    <svg class="m-6 h-52" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                      </svg>
+                    
+                    <a href="product" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">Product</a>
+    
+                </div>
+                <div class="flex flex-col bg-pixie-green-500 rounded-lg shadow-md w-60 m-6 overflow-hidden sm:w-52">
+                    <svg class="m-6 h-52 " xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                      </svg>
+                    
+                    <a href="inventory" class="bg-casal-500 text-white p-3 text-center hover:bg-casal-700 transition-all duration-500">Inventory</a>
+    
+                </div>
+                
+            </div>
+            
+</div>
+
+</div>
+
+          <script src="script.js"></script> 
+@endsection
+
+
---
+++ inventory_adrian/resources/views/inventory.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.navbar')
+
+@section('content')
+        <script src="script.js"></script>
+        <div class="bg-white lg:w-3/4 mx-auto lg:px-16 xs:px-6 sm:px-6 md:px-10 pt-16 justify-center">
+
+            @include('layouts.flash')
+                
+                <form action="addinventory" method="post" class="text-gray-600 font-bold">
+                @csrf
+
+              <div class="flex">
+                  <div class="w-60 px-1">
+                      <label class="text-gray-600">PRODUCT NAME</label>
+                      <input type="text" placeholder="" name="product_name" value="{{old('product_name')}}" class="w-full mt-2 mb-2 px-4 py-2 border border-black-900 rounded-md
+                      text-gray-700 "/>
+                      <span class="text-red-800">@error('name') {{$message}} @enderror </span>
+                  </div>
+                  <div class="w-60 px-1">
+                      <label class="text-gray-600 ">ITEMS</label>
+                      <input type="text" placeholder="" name="items" value="{{old('items')}}" class="w-full mt-2 mb-2 px-2 py-2 border border-black-900 rounded-md
+                      text-gray-700 "/>
+                      <span class="text-red-800">@error('items') {{$message}} @enderror </span>
+                  </div>
+                  <div class="w-60 px-1">
+                      <label class="text-gray-600 ">QUANTITY</label>
+                      <input type="text" placeholder="" name="quantity" value="{{old('quantity')}}" class="w-full mt-2 mb-2 px-2 py-2 border border-black-900 rounded-md
+                      text-gray-700 "/>
+                      <span class="text-red-800">@error('quantity') {{$message}} @enderror </span>
+                  </div>
+                  <div class="px-2 mt-0">
+                    <button class="lg:w-24 xs:w-24 sm:w-24 md:w-24 mt-8 mb-2 px-4 py-2 border rounded-md bg-blaze-orange-500 text-white font-bold ">ADD</button>
+                </div>
+                </form>
+              </div>
+              <br>
+              <br>
+              <br>
+             <!--TABLE-->
+             <table class=" shadow-md border-collapse w-full">
+              <thead>
+                  <tr>
+                      <th class="p-3 font-bold text-sm uppercase bg-shark-500 text-white border border-gray-300 hidden lg:table-cell">#</th>
+                      <th class="p-3 font-bold text-sm uppercase bg-shark-500 text-white border border-gray-300 hidden lg:table-cell">PRODUCT NAME</th>
+                      <th class="p-3 font-bold text-sm uppercase bg-shark-500 text-white border border-gray-300 hidden lg:table-cell">Items</th>
+                      <th class="p-3 font-bold text-sm uppercase bg-shark-500 text-white border border-gray-300 hidden lg:table-cell">Quantity</th>
+                      <th class="p-3 font-bold text-sm uppercase bg-shark-500 text-white border border-gray-300 hidden lg:table-cell">Actions</th>
+                  </tr>
+              </thead>
+              <tbody>
+              @foreach ($list as $item)
+                  <tr class="bg-hint-of-green-500 lg: flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+                    <td class="w-full lg:w-auto p-1.5 text-gray-800 text-center border text-sm border-b block lg:table-cell  lg:static">
+                        <span class="lg:hidden float-left top-0 left-0 px-2 py-1 text-xs font-bold uppercase">#</span>
+                        {{$item->id}}
+                    </td>
+                    <td class="w-full lg:w-auto p-1.5 text-gray-800 text-center border text-sm border-b block lg:table-cell  lg:static">
+                        <span class="lg:hidden float-left top-0 left-0 px-2 py-1 text-xs font-bold uppercase">Product Code</span>
+                        {{$item->product_name}}
+                    </td>
+                    <td class="w-full lg:w-auto p-1.5 text-gray-800 border border-b text-sm text-center block lg:table-cell  lg:static">
+                        <span class="lg:hidden float-left top-0 left-0 px-2 py-1 text-xs font-bold uppercase">Items</span>
+                        {{$item->items}}
+                      </td>
+                      <td class="w-full lg:w-auto p-1.5 text-gray-800 border border-b text-sm text-center block lg:table-cell  lg:static">
+                        <span class="lg:hidden float-left top-0 left-0 px-2 py-1 text-xs font-bold uppercase">Quantity</span>
+                        {{$item->quantity}}
+                      </td>
+                    <td class="w-full lg:w-auto p-1.5 text-gray-800  border border-b text-sm text-center lg:table-cell  lg:static">
+                        <span class="lg:hidden float-left top-0 left-0 px-2 py-1 text-xs font-bold uppercase">Actions</span>
+                        <a href="editinventory/{{ $item->id }}" class="text-blue-400 hover:text-blue-600 underline"><svg xmlns="http://www.w3.org/2000/svg" 
+                            class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="black"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                          </svg></a>
+                        <a href="inventorydelete/{{ $item->id }}" class="text-blue-400 hover:text-blue-600 underline pl-6"><svg xmlns="http://www.w3.org/2000/svg" 
+                            class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="black"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2
+                             2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                          </svg></a>
+                    </td>
+                </tr>
+                @endforeach
+                
+              </tbody>
+          </table>
+@endsection
