e422e64a1752e6b8620225afe206dc3f6d7a6a38
KentJosephAlgar <89622322+KentJosephAlgar@users.noreply.github.com>
2021-10-12 17:26:14 +0800

    Add files via upload

---
+++ EverCab_ALGAR/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ReservationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/login', [CustomAuthController::class, 'login'])->middleware('alreadyLoggedIn');
+
+Route::get('/register', [CustomAuthController::class, 'register'])->middleware('alreadyLoggedIn');
+
+Route::post('/login-user', [CustomAuthController::class, 'loginUser'])->name('login-user');
+
+Route::post('/register-user', [CustomAuthController::class, 'registerUser'])->name('register-user');
+
+Route::get('/welcome', [CustomAuthController::class, 'welcome']);
+
+Route::get('/about', [CustomAuthController::class, 'about']);
+
+Route::get('/dashboard', [CustomAuthController::class, 'dashboard'])->middleware('isLoggedIn');
+
+Route::get('/notifset', [CustomAuthController::class, 'notifset'])->middleware('isLoggedIn');
+
+Route::get('/privset', [CustomAuthController::class, 'privset'])->middleware('isLoggedIn');
+
+Route::get('/payments', [CustomAuthController::class, 'payments'])->middleware('isLoggedIn');
+
+Route::get('/payset', [CustomAuthController::class, 'payset'])->middleware('isLoggedIn');
+
+Route::get('/profile', [CustomAuthController::class, 'profile'])->middleware('isLoggedIn');
+
+Route::get('/profset', [CustomAuthController::class, 'profset'])->middleware('isLoggedIn');
+
+Route::get('/reservationadd', [ReservationController::class, 'addReservation'])->name('addreservation');
+
+Route::post('/reservationadd', [ReservationController::class, 'saveReservation'])->name('savereservation');
+
+Route::get('/reservationlist', [ReservationController::class, 'listReservation'])->name('listreservation');
+
+Route::get('/reservationedit/{id}', [ReservationController::class, 'editReservation'])->name('editreservation');
+
+Route::get('/reservationcancel/{id}', [ReservationController::class, 'cancelReservation'])->name('cancelreservation');
+
+Route::post('/reservationupdate', [ReservationController::class, 'updateReservation'])->name('updatereservation');
+
+Route::get('/logout', [CustomAuthController::class, 'logout']);
