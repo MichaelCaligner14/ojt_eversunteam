3802163f38b3b48f2f3043f3c43b5ad7c2308857
Mark Anthony Namoca <89622685+kramiieee@users.noreply.github.com>
2021-10-13 17:16:58 +0800

    Add files via upload

---
+++ Clinic_Management_System_NAMOCA/public/js/payment.js
@@ -0,0 +1,15 @@
+
+		$(document).ready(function(){
+    	// Get value on keyup funtion
+    	$("#price, #qty").keyup(function(){
+
+    	var total=0;    	
+    	var x = Number($("#price").val());
+    	var y = Number($("#qty").val());
+    	var total=x - y;  
+
+    	$('#change').val(total);
+
+    });
+});
+
