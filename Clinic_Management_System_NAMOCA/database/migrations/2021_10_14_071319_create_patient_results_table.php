bc1dedde43620f6408d7fb67dbf59918c7bb6fc0
Mark Anthony Namoca <89622685+kramiieee@users.noreply.github.com>
2021-10-14 17:33:58 +0800

    Add files via upload

---
+++ Clinic_Management_System_NAMOCA/database/migrations/2021_10_14_071319_create_patient_results_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patient', function (Blueprint $table) {
+            $table->increments('id');
+            $table -> string ('fullname');
+            $table -> string ('address');
+            $table -> string ('email');
+            $table -> string ('contact');
+            $table -> string ('age');
+            $table ->  string ('birth');
+            $table -> string ('temp');
+            $table -> string ('weight');
+            $table -> string ('height');
+            $table ->timestamps();
+        });
+
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patientID');
+            $table->foreign('patientID')->references('id')->on('patient')->onDelete('cascade');
+            $table -> string ('time');
+            $table -> string ('test');
+            $table->timestamps();
+        });
+
+        Schema::create('payment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patientID');
+            $table->foreign('patientID')->references('id')->on('results')->onDelete('cascade');
+            $table -> string ('bill');
+            $table -> string ('cash');
+            $table -> string ('change');
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patient_results');
+    }
+}
