35b2715dcd9642a5e00176ec982ba2959fbbba3b
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-10-21 17:28:17 +0800

    Add files via upload

--- payroll_MichaelC/database/migrations/2021_10_12_074918_create_employees_table.php
+++ payroll_MichaelC/database/migrations/2021_10_12_074918_create_employees_table.php
@@ -11,42 +11,42 @@ class CreateEmployeesTable extends Migration
      *
      * @return void
      */
+
     public function up()
     {
-        Schema::create('empAdd', function (Blueprint $table) {
-            $table->increments('id')->from(101);
+        
+        Schema::create('empadd', function (Blueprint $table) {
+            $table->id()->from(101)->unique();
             $table->string('name');
             $table->string('address');
             $table->integer('age');
             $table->string('sex');
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
-        Schema::create('empAttendance', function (Blueprint $table) {
-            $table->increments('id')->from(2001);
-            $table->unsignedInteger('empAdd_id');
-            $table->foreign('empAdd_id')->references('id')->on('empAdd')->onDelete('cascade');
+        Schema::create('empattendance', function (Blueprint $table) {
+            $table->id()->from(2001);
+            $table->unsignedBigInteger('empadd_id');
+            $table->foreign('empadd_id')->references('id')->on('empadd')->onDelete('cascade')->unique();
             $table->string('datefrom');
             $table->string('dateto');
             $table->integer('rate');
             $table->integer('absent');
             $table->integer('workdays');
             $table->integer('initialamount');
-            $table->timestamps();
+            $table->integer('total_salary');
+             $table->timestamp('created_at')->useCurrent();
+             $table->timestamp('updated_at')->useCurrent();
         });
         Schema::create('empDeductionSalary', function (Blueprint $table) {
-            $table->increments('id')->from(30001);
-            $table->unsignedInteger('empattendance_id');
-            $table->foreign('empattendance_id')->references('id')->on('empAttendance')->onDelete('cascade');
+            $table->id()->from(30001);
+            $table->unsignedBigInteger('empattendance_id');
+            $table->foreign('empattendance_id')->references('id')->on('empattendance')->onDelete('cascade')->unique();
             $table->string('typeDeduction');
             $table->integer('deductionAmount');
-            $table->timestamps();
-        });
-        Schema::create('TotalAmount', function (Blueprint $table) {
-            $table->increments('id')->from(400001);
-            $table->unsignedInteger('empdeductionSalary_ID');
-            $table->foreign('empdeductionSalary_ID')->references('id')->on('empDeductionSalary')->onDelete('cascade');
-            $table->integer('TotalSalary');
-            $table->timestamps();
+            $table->decimal('deducted_salary')->default(0);
+             $table->timestamp('created_at')->useCurrent();
+             $table->timestamp('updated_at')->useCurrent();
         });
     }
 
