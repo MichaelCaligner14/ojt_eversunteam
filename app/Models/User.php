c794a8d1f9841139aee8bb961fca438fa69c27ea
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-10-22 17:27:51 +0800

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Authenticable
+{
+    use HasFactory;
+    
+}
