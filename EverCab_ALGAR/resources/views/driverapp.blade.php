bf19eaa6520fbb1a2e1b7678c4bd6837f266abc0
KentJosephAlgar <89622322+KentJosephAlgar@users.noreply.github.com>
2021-10-22 17:37:53 +0800

    Add files via upload

---
+++ EverCab_ALGAR/resources/views/driverapp.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EverCab Drivers</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body class="bg-light-gray">
+    <span>hello world</span>
+</body>
+</html>
