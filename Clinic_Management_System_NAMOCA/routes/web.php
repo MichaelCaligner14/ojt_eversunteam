efb978202eebaadef55828e06c21a8da5c3b90a8
Mark Anthony Namoca <89622685+kramiieee@users.noreply.github.com>
2021-10-29 17:07:23 +0800

    Add files via upload

--- Clinic_Management_System_NAMOCA/routes/web.php
+++ Clinic_Management_System_NAMOCA/routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\patientController;
 use App\Http\Controllers\resultController;
 use App\Http\Controllers\appointmentController;
 use App\Http\Controllers\paymentController;
+use App\Events\Message;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 /*
 |--------------------------------------------------------------------------
@@ -27,37 +30,15 @@ Route::get('/sign',[LogInController::class,'sign']);
 Route::post('/sign-user',[LogInController::class,'signUser']) ->name('sign-user');
  Route::post('/login-user',[LogInController::class,'loginUser']) ->name('login-user');
 
-
-
-
-
-
-
- Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/index');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-
-
-
-
-
-
-
-
-
+ Route::get('/welcome',[LogInController::class,'welcome']);
+ Route::post('/send-message', function(Request $request){
+     event(new Message(
+         $request->input('username'),
+         $request->input('message')
+     )
+ );
+ return ["sucess" => true];
+ });
 
 
 
