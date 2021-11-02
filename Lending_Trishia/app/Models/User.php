a0798cb04bbc725caca4e96a1a543456fc21733c
TrishiaHamo-ay <89622810+TrishiaHamo-ay@users.noreply.github.com>
2021-11-02 23:24:27 +0800

    Update User.php

--- Lending_Trishia/app/Models/User.php
+++ Lending_Trishia/app/Models/User.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
 }
