c7e09347eb5b3de8b60e2b8f827454662af40c49
Mark Anthony Namoca <89622685+kramiieee@users.noreply.github.com>
2021-11-03 09:19:11 +0800

    Add files via upload

--- Clinic_Management_System_NAMOCA/app/Models/user.php
+++ Clinic_Management_System_NAMOCA/app/Models/user.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class user extends Authenticatable implements MustVerifyEmail
+class user extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory,Notifiable;
+    
 }
---
+++ Clinic_Management_System_NAMOCA/app/Notifications/Verify.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class Verify extends Notification
+{
+    use Queueable;
+    private $varify;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($varify)
+    {
+        $this->varify = $varify;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                            ->line($this->varify['body'])
+                            ->action($this->varify['verifyEmail'], $this->varify['url'])
+                            ->line($this->varify['thankyou']);
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
