341aab5d6c4067cb2e41dbe921753851016906cf
pinedajamesryan09 <89622639+pinedajamesryan09@users.noreply.github.com>
2021-11-05 17:50:40 +0800

    Add files via upload

---
+++ student_registration_James/resources/views/mail/mails.blade.php
@@ -0,0 +1,84 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start Page</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body>
+
+
+       
+<body class="background-color:coral; ">
+ 
+    <div
+      class="px-96 w-screen h-screen flex flex-col justify-center items-center lg:grid lg:grid-cols-2"
+    >
+    
+      
+      
+      <br>
+      <br>
+      <h1
+          class="my-8 font-roboto font-bold text-3xl text-white text-center hover:text-blue-700 "
+        >
+        
+     Student Registration!
+        </h1>
+        
+          <div class="flex mt-9 mx-28 mr-1 text-white text-center font-bold">
+            
+              <span class="ml-2">You receive a new notification</span>
+          
+          </div>
+          
+        <br>
+        <a href="http://localhost/studentregistration/public/homepage"
+          class="py-1 px-5 bg-primarycolor rounded-7 mx-28 text-white font-bold hover:bg-red-400 uppercase text-lg mt-4 transform bg-black-600 transition-all duration-500 bg-red-600 bg-opacity-90 rounded" style="width: 125px; height: 100px;"
+          >You Have Successfully Registered!</a
+        > 
+<br>
+<br>
+            <h2
+          class="my-8 font-roboto font-bold text-3xl text-white text-center hover:text-blue-700 "
+        >
+        
+        Welcome!	😄
+        </h2>
+        
+      <br>  
+      <br>
+      <br>
+      
+        
+         <span class="ml-36">© 2021 Eversun Software. All rights reserved.</span>
+
+
+        
+        </div>
+        <Br>
+      
+    
+<br>
+    
+
+
+  
+      </form>
+    
+    </div>
+    
+
+   
+
+    </div>
+</div>
+</div>
+
+<script src="./js/app.js"></script>
+    
+</body>
+</html>
