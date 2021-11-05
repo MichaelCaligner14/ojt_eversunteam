aa5cdaf4bafee621ebe326fe99845e7fb287d10e
MichaelCaligner14 <89622607+MichaelCaligner14@users.noreply.github.com>
2021-11-05 17:49:33 +0800

    Add files via upload

--- payroll_MichaelC/database/migrations/2014_10_12_000000_create_users_table.php
+++ payroll_MichaelC/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('lastname');
+            $table->string('firstname');
             $table->string('username');
             $table->string('password');
             $table->string('email')->unique();
