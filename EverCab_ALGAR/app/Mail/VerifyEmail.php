42bb721094a4aeada54b967fe3022cec43e7b8ab
KentJosephAlgar <89622322+KentJosephAlgar@users.noreply.github.com>
2021-11-09 17:27:52 +0800

    Add files via upload

---
+++ EverCab_ALGAR/app/Mail/VerifyEmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->verify_mail_data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->from('no-reply.EverCab@eversun.com')
+            ->subject('Welcome to EverCab!')
+            ->view('mail.verify-email', ['mail_data' => $this->verify_mail_data]);
+    }
+}
